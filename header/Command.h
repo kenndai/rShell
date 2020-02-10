70cd2abcdbbcffb6c2714e1a6fd2448d6d1b8c5a
kenndai <heyitskennydai@gmail.com>
2020-02-09 20:25:12 -0800

    Added syscall logic to command class

--- header/Command.h
+++ header/Command.h
@@ -1,7 +1,11 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include "unistd.h"
 #include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 class Command {
 
@@ -13,14 +17,14 @@ class Command {
         bool checkExit();
 
     private:
-
+	bool executed = false;
         bool exit = false;
         std::string info[2];
 };
 
 Command::Command(std::string *info) {
-    this->info[0] = info[0];
-    this->info[1] = info[1];
+    this->info[0] = info[0]; //executable
+    this->info[1] = info[1]; //argument list
 }
 
 Command::~Command() {
@@ -29,10 +33,35 @@ Command::~Command() {
 
 bool Command::run() {
     //fork(), execvp(), waitpid()
-    //if exit command was called set exit variable to true
-    //check wifexit() if successful or not, return true or false
+    int status;
+    char* args[] = {(char*)info[0].c_str(), (char*)info[1].c_str(), NULL}; //create char* array based on string array
+
+    pid_t cPid = fork(); //forks the process
+
     std::cout << info[0] << " " << info[1] << std::endl;    //just for testing purposes to see which commands were called
-    return true;
+
+    if (cPid == 0) { //child process does the executing
+	execvp(args[0], args);	//executes the command
+    }
+    else if (cPid > 0) {
+ 	waitpid(cPid, &status, 0); //if parent wait for child to terminate	
+	if (WIFEXITED(status)) { //if process TERMINATES successfully
+	   int exitStatus = WEXITSTATUS(status);
+	   std::cout << "Child process exit status: " << exitStatus << std::endl;
+	   if (exitStatus == 0) { //if command was EXECUTED successfully
+		/*if a nonexecutable is at args[0] and execvp is called on it, exit status is still 0*/
+		executed = true;	 
+	   }
+	}
+    }
+    else {
+	std::cout << "fork() failed" << std::endl;
+	exit = 1; //if exit command was called set exit variable to true
+	_exit(1); //exit when fork fails	
+    }
+    // return true; /*original*/
+    //return true if the COMMAND was run successfully
+    return executed;   
 }
 
 bool Command::checkExit() {
