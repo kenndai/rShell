18a26b236710e5537094b729983ec3dfba50784b
alexandertrinh <alexander9821@gmail.com>
2020-02-13 22:16:10 -0800

    read.h

---
+++ header/Read.h
@@ -0,0 +1,18 @@
+fine READ_H
+
+#include <string>
+
+class Read {
+
+    public:
+        Read() {};
+        ~Read() {};
+
+    private:
+
+        virtual void trimWhiteSpaces(std::string &str) = 0;
+
+};
+
+#endif // READ_H
+
