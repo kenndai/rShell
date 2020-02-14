a81f43babeac556f83c0bcdf9a49d38b40d383cc
kenndai <heyitskennydai@gmail.com>
2020-02-13 17:51:44 -0800

    Added additional tests for Command and Token

--- unit_tests/Command_test.hpp
+++ unit_tests/Command_test.hpp
@@ -14,4 +14,18 @@ TEST(CommandTest, CommandEchoHelloWorld) {
 	EXPECT_EQ(command.run(), true);
 }
 
+TEST(CommandTest, CommandInvalid) {
+	string info[2] = {"echaaa", "hello world"};
+	string* ptr = info;
+	Command command(ptr);
+	EXPECT_EQ(command.run(), false);
+}
+
+TEST(CommandTest, CommandLs_a) {	
+	string info[3] = {"ls", "-a", "-l"};
+	string* ptr = info;
+	Command command(ptr);
+	EXPECT_EQ(command.run(), true);
+}
+
 #endif
--- unit_tests/Token_test.hpp
+++ unit_tests/Token_test.hpp
@@ -10,23 +10,20 @@ TEST(TokenTest, TokenGetString) {
 	Token test = Token(cmd);	
 	EXPECT_EQ(test.getString(), "hello world");
 }
-/*
-TEST(TokenTest, TokenGetInfo) {
-	string cmd = "hello world";
-	Token test = Token(cmd);
-	EXPECT_EQ(test.getInfo(), );
-}
-
-TEST(TokenTest, TokenQuotations) {
-	string cmd = "\"hello world\"";
-	Token test = Token(cmd);
-	EXPECT_EQ(test.getInfo(), );
-}
-
-TEST(TokenTest, TokenComments) {
-	string cmd = "hell#o world";
+TEST(TokenTest, GetStringComment) {
+	string cmd = "hello# world";
 	Token test(cmd);
-	EXPECT_EQ(test.getInfo(), );
+	EXPECT_EQ(test.getString(), "hello# world");
 }
-*/
+TEST(TokenTest, GetInfoNotNULL) {
+	string cmd = "hello world";
+	Token test(cmd);
+	EXPECT_TRUE(test.getInfo() != nullptr);
+}
+TEST(TokenTest, GetInfoString) {
+	string cmd = "hello world";
+	Token test(cmd);
+	string first = *(test.getInfo());
+	EXPECT_EQ(first, "hello");
+}
 #endif
