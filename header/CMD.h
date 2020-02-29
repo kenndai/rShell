20cf0c9f5e3f329133dfd1e60f82fdb2d7eedd15
kenndai <heyitskennydai@gmail.com>
2020-02-28 19:44:37 -0800

    reimplemented test in CMD again

--- header/CMD.h
+++ header/CMD.h
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <errno.h>
 
 class CMD : public Token{
@@ -40,7 +41,7 @@ class CMD : public Token{
         std::string& removeBrackets(std::string &str);
         void convertToTestCmd();
         void checkEcho();
-
+	bool runTest();
 };
 
 CMD::CMD(std::string cmdStr) {
@@ -134,6 +135,7 @@ std::string& CMD::removeQuotations(std::string &str) {
 
 bool CMD::execute() {
     int status;
+    bool testStatus;
 
     char* args[3] = {NULL, NULL, NULL};
     for (unsigned int i = 0; i < 3; i++) {
@@ -147,6 +149,17 @@ bool CMD::execute() {
 
         if ( (info[0] == "exit") || (info[0] == "") ) //if empty command or "exit()" just terminate
             exit(0);
+	else if ( info[0] == "test" ) {
+	    testStatus = runTest();
+	    if (testStatus) { //if path exists
+		std::cout << "(True)" << std::endl;
+		exit(0);
+	    }
+	    else {
+		std::cout << "(False)" << std::endl;
+		exit(1);
+	    }
+	}
         else if ( execvp(args[0], args) == -1 ) { //checks for valid command to execute, exit status is 1 if nonvalid command passed in
             std::cout << "-bash: " << info[0] << ": command not found" << std::endl;
             exit(1); //exit() sets the status to 1 for parent fork
@@ -176,6 +189,32 @@ bool CMD::execute() {
         std::cout << "cPid = -1, fork() failed" << std::endl; //testing purposes
 
     return false; //return true if the COMMAND was run successfully
+}
+
+bool CMD::runTest() {
+    struct stat buffer;
+    mode_t fileMode;
+
+    stat((char*)info[2].c_str(), &buffer); //pass in path, store info in buffer
+    fileMode = buffer.st_mode; //returns mode_t of stat
+    bool isReg = false;
+    bool isDir = false;
+
+    if ( info[1] == "-e" || info[1] == "-f" || info[1] == "-d" ) {
+	isReg = S_ISREG(fileMode);
+	isDir = S_ISDIR(fileMode);
+	if (info[1] == "-e") 
+	    return (isReg || isDir);
+	else if (info[1] == "-f")
+	    return isReg;
+	else
+	    return isDir;	    
+    }	
+    else {
+	std::cout << "-bash: test: " << info[1] << ": unary operator expected" << std::endl;
+	return false;
+    }
+
 }
 
 std::string CMD::tokenType() {
